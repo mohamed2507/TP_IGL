861c793dd045171e8dfb91c377db4fcd9f17ec9b
mohamed2507 <mohamed25071999@gmail.com>
2019-12-30 19:51:43 +0100

    test unitaire

---
+++ service_admin/tests/Unit/FormTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Administrateur;
+use App\Etudiant;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Illuminate\Http\Response;
+
+class FormTest extends TestCase
+{
+     use RefreshDatabase;
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+             $this->post('api/admin/register',['nom'=>"benmaiza","prenom"=>"mohamed","sexe"=>'Male',"dateN"=>'2000-12-12','adresse'=>'alger']);
+   $this->assertCount(1,Administrateur::all());
+      /* $respons=$this->get('/form-admin');
+        $respons->assertStatus(200);//http status code
+       /* $etudiant = Etudiant::create(['nom'=>"benmaiza","prenom"=>"mohamed","sexe"=>'Male',"dateN"=>'2000-12-12','promo'=>'1 CP']);
+        //$this->assertEquals("benmaiza mohamed Male 2000-12-12 1 CP",$etudiant->full_account());
+        $this->assertDatabaseHas('etudiants',['nom'=>"benmaiza","prenom"=>"mohamed","sexe"=>'Male',"dateN"=>'2000-12-12','promo'=>'1 CP']);
+        /*$respons=$this->post('/etudiant/register',['nom'=>"benmaiza","prenom"=>"mohamed","sexe"=>'Male',"dateN"=>'2000-12-12','promo'=>'1 CP']);
+        $respons->assertStatus(302);*/
+
+
+      /*  $respons=$this->get('/formulaire-admin');
+        $respons->assertStatus(200);//http status code
+        $admin = Administrateur::create(['nom'=>"benmaiza","prenom"=>"mohamed","sexe"=>'Male',"dateN"=>'2000-12-12','adresse'=>'alger']);
+        $this->assertDatabaseHas('administrateurs',['nom'=>"benmaiza","prenom"=>"mohamed","sexe"=>'Male',"dateN"=>'2000-12-12','adresse'=>'alger']);//verifier la base de donnés
+        /*$respons=$this->post('/admin/register',['nom'=>"benmaiza","prenom"=>"mohamed","sexe"=>'Male',"dateN"=>'2000-12-12','adresse'=>'alger',"_token"=>$token  ]);
+        $respons->assertStatus(302);*/
+        }
+}
